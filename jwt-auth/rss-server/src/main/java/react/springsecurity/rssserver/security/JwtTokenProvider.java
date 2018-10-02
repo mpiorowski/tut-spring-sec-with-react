6ff7b8ba919670b47adf7c0f6ac02adc059ecc8a
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2018-10-02 13:48:49 +0200

    refreshed for java 10, working

--- jwt-auth/rss-server/src/main/java/react/springsecurity/rssserver/security/JwtTokenProvider.java
+++ jwt-auth/rss-server/src/main/java/react/springsecurity/rssserver/security/JwtTokenProvider.java
@@ -1,12 +1,14 @@
 package react.springsecurity.rssserver.security;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -14,11 +16,14 @@ public class JwtTokenProvider {
 
   private static final Logger logger = LoggerFactory.getLogger(JwtTokenProvider.class);
 
-  @Value("${app.jwtSecret}")
-  private String jwtSecret;
+//  not used
+//  @Value("${app.jwtSecret}")
+//  private String jwtSecret;
 
   @Value("${app.jwtExpirationInMs}")
   private int jwtExpirationInMs;
+
+  private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
   public String generateToken(Authentication authentication) {
 
@@ -27,27 +32,26 @@ public class JwtTokenProvider {
     Date now = new Date();
     Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
 
+
     return Jwts.builder()
         .setSubject(Long.toString(userPrincipal.getId()))
         .setIssuedAt(new Date())
         .setExpiration(expiryDate)
-        .signWith(SignatureAlgorithm.HS512, jwtSecret)
+        .signWith(key)
         .compact();
   }
 
   public Long getUserIdFromJWT(String token) {
-    Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+    Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
 
     return Long.parseLong(claims.getSubject());
   }
 
   public boolean validateToken(String authToken) {
     try {
-      Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+      Jwts.parser().setSigningKey(key).parseClaimsJws(authToken);
       return true;
-    } catch (SignatureException ex) {
-      logger.error("Invalid JWT signature");
-    } catch (MalformedJwtException ex) {
+    }  catch (MalformedJwtException ex) {
       logger.error("Invalid JWT token");
     } catch (ExpiredJwtException ex) {
       logger.error("Expired JWT token");
